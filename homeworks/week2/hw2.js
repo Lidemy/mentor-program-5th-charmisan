8995f84a982a41090a3dba184dc4c4dfd26511c3
Charisma <charisma@Charismade-MacBook-Pro.local>
2021-04-25 02:49:09 +0800

    wee2_hw

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,7 @@
 function capitalize(str) {
-  
+  str2 = str[0].toUpperCase()
+  str3 = str.substring(1)
+  return str2 + str3
 }
 
 console.log(capitalize('hello'));
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,19 @@
 function join(arr, concatStr) {
-  
+  var n = arr.length
+  if ( n === 1 ) return arr[0]
+  var new_arr = '' 
+  for(var i = 0; i < n-1; i++ ){
+    new_arr +=  arr[i] + concatStr
+  }
+  return new_arr + arr[n-1]   
 }
 
 function repeat(str, times) {
-  
+  var new_arr = ''
+  for(var i = 0; i < times; i++){
+    new_arr += str
+  }
+  return new_arr   
 }
 
 console.log(join(['a'], '!'));
