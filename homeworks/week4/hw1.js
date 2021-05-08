a9c23a18d26ec6d10f6853d960d14deb3c7c296c
Charisma <charisma@Charismade-MacBook-Pro.local>
2021-05-08 21:52:58 +0800

    hw1

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -3,6 +3,19 @@ const request = require('request')
 request(
   'https://lidemy-book-store.herokuapp.com/books?_limit=10',
   (error, response, body) => {
-    console.log('body:', body)
+    if (error) {
+      console.log(error)
+      return
+    }
+    let books
+    try {
+      books = JSON.parse(body)
+    } catch (error) {
+      console.log(error)
+      return
+    }
+    for (let i = 0; i < books.length; i++) {
+      console.log(books[i].id + books[i].name)
+    }
   }
 )
